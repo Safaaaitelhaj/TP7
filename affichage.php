4f995b541224cb4ae395542e9a9dfdbdb746ecdd
Safaaaitelhaj <safaaitgst@gmail.com>
2025-05-04 19:36:02 +0200

    Update affichage.php

--- affichage.php
+++ affichage.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style2.css">
 </head>
 <body>
-
+<center>
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nom = $_POST['nom'];
@@ -18,17 +17,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $date_fin = $_POST['date_fin'];
     $encadrement = $_POST['encadrement'];
     
-    echo "<h2>Informations soumises :</h2>";
-    echo "<p><strong>Nom :</strong> " . $nom . "</p>";
-    echo "<p><strong>Prénom :</strong> " . $prenom . "</p>";
-    echo "<p><strong>Groupe :</strong> " . $group . "</p>";
-    echo "<p><strong>Sujet :</strong> " . $sujet . "</p>";
-    echo "<p><strong>Date début :</strong> " . $date_debut . "</p>";
-    echo "<p><strong>Date fin :</strong> " . $date_fin . "</p>";
-    echo "<p><strong>Encadrement :</strong> " . $encadrement . "</p>";
+    echo "<h1>Informations  :</h1>";
+    echo "<p><strong>Nom :</strong> " . $nom . "</p></br>";
+    echo "<p><strong>Prénom :</strong> " . $prenom . "</p></br>";
+    echo "<p><strong>Groupe :</strong> " . $group . "</p></br>";
+    echo "<p><strong>Sujet :</strong> " . $sujet . "</p></br>";
+    echo "<p><strong>Date début :</strong> " . $date_debut . "</p></br>";
+    echo "<p><strong>Date fin :</strong> " . $date_fin . "</p></br>";
+    echo "<p><strong>Encadrement :</strong> " . $encadrement . "</p></br>";
 } else {
     echo "<p>Aucune donnée soumise.</p>";
 }
 ?>
+</center>
+
 </body>
 </html>
